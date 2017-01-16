b769a8cf87d58e95d5e1a7c669b3c299f391d67d
yangchunke <yangchunke@yahoo.com>
2017-01-16 11:32:36 -0800

    updated pom dependencies.

--- src/main/java/net/yck/wrkdb/core/DBException.java
+++ src/main/java/net/yck/wrkdb/core/DBException.java
@@ -1,18 +1,18 @@
 package net.yck.wrkdb.core;
 
-public class DBException extends Exception {
+public class DBException extends RuntimeException {
 
-    /**
-     * 
-     */
-    private static final long serialVersionUID = 168044513209548724L;
-    
-    public DBException(String msg){
-        super(msg);
-    }
-    
-    public DBException(Exception inner){
-        super(inner);
-    }
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 168044513209548724L;
+
+  public DBException(String msg) {
+    super(msg);
+  }
+
+  public DBException(Exception inner) {
+    super(inner);
+  }
 
 }
--- src/main/java/net/yck/wrkdb/meta/Key.java
+++ src/main/java/net/yck/wrkdb/meta/Key.java
@@ -1,32 +1,32 @@
 package net.yck.wrkdb.meta;
 
-import org.mapdb.DBException;
+import net.yck.wrkdb.core.DBException;
 
 abstract class Key extends Group {
 
-    public final static class Partition extends Key {
+  public final static class Partition extends Key {
 
-        public Partition() {
-            super.setName(kw_PartitionKey);
-        }
-
-        @Override
-        public NamedElement setName(String name) {
-            throw new DBException("The name of partition key cannot be changed.");
-        }
+    public Partition() {
+      super.setName(kw_PartitionKey);
     }
 
-    public final static class Row extends Key {
-        public Row() {
-            super.setName(kw_RowKey);
-        }
+    @Override
+    public NamedElement setName(String name) {
+      throw new DBException("The name of partition key cannot be changed.");
+    }
+  }
 
-        @Override
-        public NamedElement setName(String name) {
-            throw new DBException("The name of row key cannot be changed.");
-        }
+  public final static class Row extends Key {
+    public Row() {
+      super.setName(kw_RowKey);
     }
 
-    public final static class Clustering extends Key {
+    @Override
+    public NamedElement setName(String name) {
+      throw new DBException("The name of row key cannot be changed.");
     }
+  }
+
+  public final static class Clustering extends Key {
+  }
 }
